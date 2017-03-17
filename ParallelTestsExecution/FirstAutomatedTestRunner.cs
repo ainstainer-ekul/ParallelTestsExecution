661fbaba99c55bbd788e9b240429af961d8ef4e3
evgeniy.kulikov <evgeniy.kulikov@ainstainer.de>
2017-03-17 17:39:56 +0200

    init commit

---
+++ ParallelTestsExecution/FirstAutomatedTestRunner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace ParallelTestsExecution
+{
+    [TestFixture]
+    [Parallelizable]
+
+    public class FirstAutomatedTestRunner
+    {
+        private static String STAGING_URL = "https://staging.university4industry.com";
+        private RemoteWebDriver remoteChrome;
+
+        [SetUp]
+        public void Init()
+        {
+            DesiredCapabilities remoteChromeCapability = DesiredCapabilities.Chrome();
+            remoteChromeCapability.SetCapability("version", "");
+            remoteChromeCapability.SetCapability("platform", "Linux");
+            remoteChrome = new RemoteWebDriver(new Uri("http://192.168.99.100:32776/wd/hub"), remoteChromeCapability);
+
+            remoteChrome.Manage().Window.Maximize();
+            remoteChrome.Navigate().GoToUrl(STAGING_URL);
+        }
+
+        [Test]
+        public void HartingPageIsDisplayedAfterNavigateOnHartingPage()
+        {
+            remoteChrome.Navigate().GoToUrl("https://staging.university4industry.com/harting");
+            Assert.AreEqual(STAGING_URL + "/harting", remoteChrome.Url);
+        }
+
+        [Test]
+        public void DialogRegisterIsDisplayedAfterNavigateOnRegisterPage()
+        {
+            remoteChrome.Navigate().GoToUrl("https://staging.university4industry.com/?dialog=register");
+            Assert.AreEqual(STAGING_URL + "/?dialog=register", remoteChrome.Url);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            remoteChrome.Quit();
+        }
+    }
+}
---
+++ ParallelTestsExecution/SecondAutomatedTestRunner.cs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace ParallelTestsExecution
+{
+    [TestFixture]
+    [Parallelizable]
+    public class SecondAutomatedTestRunner
+    {
+        private static String STAGING_URL = "https://staging.university4industry.com";
+        private RemoteWebDriver remoteFirefox;
+
+        [SetUp]
+        public void Init()
+        {
+            DesiredCapabilities remoteFirefoxCapability = DesiredCapabilities.Firefox();
+            remoteFirefoxCapability.SetCapability("version", "");
+            remoteFirefoxCapability.SetCapability("platform", "Linux");
+            remoteFirefox = new RemoteWebDriver(new Uri("http://192.168.99.100:32776/wd/hub"), remoteFirefoxCapability);
+            remoteFirefox.Navigate().GoToUrl(STAGING_URL);
+        }
+
+        [Test]
+        public void UrLHasDiscoverWordAfterPressOnDiscoverButton()
+        {
+            By discoverButton = By.XPath("//button//*[text()='Discover']");
+            remoteFirefox.FindElement(discoverButton).Click();
+            Assert.AreEqual(STAGING_URL + "/discover", remoteFirefox.Url);
+        }
+
+        [Test]
+        public void UrLHasDiscussWordAfterPressOnDiscussButton()
+        {
+            By discussButton = By.XPath("//button//*[text()='Discuss']");
+            remoteFirefox.FindElement(discussButton).Click();
+            Assert.AreEqual(STAGING_URL + "/?dialog=login", remoteFirefox.Url);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            remoteFirefox.Quit();
+        }
+    }
+}
